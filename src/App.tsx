a17c9533de1efe14ce6d3be76ddb8e66244edd2f
Alicia Foureaux <aliciafoureauxlucas@gmail.com>
2021-02-19 22:46:55 -0300

    feat: change state

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Catalog from './components/Catalog';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   );
 }
